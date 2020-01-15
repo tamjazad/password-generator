06dddced928a89011de138cbd94a510130072bd0
Tamjeed Azad <26588289+tamjazad@users.noreply.github.com>
2020-01-14 19:30:42 -0500

    added password save functionality

--- passgen.cpp
+++ passgen.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "password.h"
 
 using namespace std;
@@ -10,5 +12,26 @@ int main()
 	cin >> length;
 	Password pass(length);
 	cout << "New password: " << pass << endl;
+	
+	cout << "Would you like to save this password? (y/n): ";
+	string choice;
+	cin >> choice;
+
+	if (choice == "y") {
+		cout << "What is this password for? Account/service: ";
+		string account;
+		cin >> account;
+		ofstream passfile;
+		passfile.open("passwords.txt", ios::app);
+		if (passfile.is_open())
+		{
+			passfile << "Password for " << account << ": " << endl;
+			passfile << pass << endl << endl;
+			cout << "Saved to 'passwords.txt'" << endl;
+			passfile.close();
+		} else
+			cout << "Unable to save password to 'passwords.txt'" << endl;
+
+	}
 	return 0;
 }
